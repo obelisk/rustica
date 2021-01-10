d006e2193b3fea0460bb0619aaf6bfe2aeb8e4a2
Mitchell Grenier <mitchell@confurious.io>
2021-01-10 17:02:23 -0500

    Changes to support different host and user keys

--- src/hsm/yubikey.rs
+++ src/hsm/yubikey.rs
@@ -13,29 +13,29 @@ pub enum Error {
     InternalYubiKeyError(yubikey_piv::error::Error),
 }
 
-fn configured(yk: &mut YubiKey) -> Result<PublicKeyInfo, Error> {
-    match yubikey_piv::certificate::Certificate::read(yk, SlotId::Retired(RetiredSlotId::R11)) {
+fn configured(yk: &mut YubiKey, slot: SlotId) -> Result<PublicKeyInfo, Error> {
+    match yubikey_piv::certificate::Certificate::read(yk, slot) {
         Ok(cert) => Ok(cert.subject_pki().clone()),
         Err(e) => Err(Error::InternalYubiKeyError(e)),
     }
 }
 
-pub fn fetch_pubkey() -> Result<PublicKeyInfo, Error> {
+fn fetch_pubkey(slot: SlotId) -> Result<PublicKeyInfo, Error> {
     let mut yubikey = match YubiKey::open() {
         Ok(yk) => yk,
         Err(e) => return Err(Error::InternalYubiKeyError(e)),
     };
-    configured(&mut yubikey)
+    configured(&mut yubikey, slot)
 }
 
-pub fn fetch_yubikey(id: Option<&[u8]>) -> Result<YubiKey, Error> {
-    match YubiKey::open() {
-        Ok(yk) => Ok(yk),
-        Err(e) => return Err(Error::InternalYubiKeyError(e)),
-    }
-}
-
-pub fn provision(yk: &mut YubiKey, pin: &[u8]) -> Result<PublicKeyInfo, Error> {
+/// This provisions the YubiKey with a new certificate. It is generally not advisable
+/// to use as this means there is no backup of the key should it be lost.
+/// It is however provided as an easy method quickly get a YubiKey properly configured
+/// for use with Rustica.
+/// 
+/// It requires that a YubiKey object be passed in a long with a pin to make sure that
+/// it is configuring the correct one.
+pub fn _provision(yk: &mut YubiKey, pin: &[u8]) -> Result<PublicKeyInfo, Error> {
     match yk.verify_pin(pin) {
         Ok(_) => (),
         Err(e) => {
@@ -62,7 +62,7 @@ pub fn provision(yk: &mut YubiKey, pin: &[u8]) -> Result<PublicKeyInfo, Error> {
     };
 
     // Generate a self-signed certificate for the new key.
-    if let Err(e) =  Certificate::generate_self_signed(
+    if let Err(e) = Certificate::generate_self_signed(
         yk,
         slot,
         [0u8; 20],
@@ -73,28 +73,87 @@ pub fn provision(yk: &mut YubiKey, pin: &[u8]) -> Result<PublicKeyInfo, Error> {
         return Err(Error::InternalYubiKeyError(e));
     }
 
-    configured(yk)
+    configured(yk, slot)
 }
 
-pub fn sign_data(data: &[u8]) -> Result<Vec<u8>, Error> {
+fn sign_data(data: &[u8], alg: AlgorithmId, slot: SlotId) -> Result<Vec<u8>, Error> {
     let mut yk = match YubiKey::open() {
         Ok(yk) => yk,
         Err(e) => return Err(Error::InternalYubiKeyError(e)),
     };
 
-    match configured(&mut yk) {
-        Ok(PublicKeyInfo::EcP256(_)) => (),
-        Ok(_) => return Err(Error::WrongKeyType),
+    let slot_alg = match configured(&mut yk, slot) {
+        Ok(PublicKeyInfo::EcP256(_)) => AlgorithmId::EccP256,
+        Ok(PublicKeyInfo::EcP384(_)) => AlgorithmId::EccP384,
+        Ok(_) => AlgorithmId::Rsa2048,  // RSAish
         Err(_) => return Err(Error::Unprovisioned),
     };
+
+    if slot_alg != alg {
+        return Err(Error::WrongKeyType);
+    }
 
     let mut hasher = Sha256::new(); 
     hasher.update(data);
 
     let hash = &hasher.finalize()[..];
 
-    match yk_sign_data(&mut yk, hash, AlgorithmId::EccP256, SlotId::Retired(RetiredSlotId::R11)) {
+    match yk_sign_data(&mut yk, hash, alg, slot) {
         Ok(sig) => Ok(sig.to_vec()),
         Err(e) => Err(Error::InternalYubiKeyError(e)),
+    }
+}
+
+/// Pull the public key from the YubiKey and wrap it in a Rustica
+/// PublicKey object.
+pub fn ssh_cert_fetch_pubkey(slot: SlotId) -> Option<rustica_sshkey::PublicKey> {
+    match fetch_pubkey(slot) {
+        //Ok(hsm::PublicKeyInfo::Rsa { pubkey, .. }) => pubkey,
+        Ok(PublicKeyInfo::EcP256(pubkey)) => {
+            let key_type = rustica_sshkey::KeyType::from_name("ecdsa-sha2-nistp256").unwrap();
+            let curve = rustica_sshkey::Curve::from_identifier("nistp256").unwrap();
+            let kind = rustica_sshkey::EcdsaPublicKey {
+                curve,
+                key: pubkey.as_bytes().to_vec(),
+            };
+
+            Some(rustica_sshkey::PublicKey {
+                key_type,
+                kind: rustica_sshkey::PublicKeyKind::Ecdsa(kind),
+                comment: None,
+            })
+        },
+        //Ok(hsm::PublicKeyInfo::EcP384(pubkey)) => pubkey.as_bytes().to_vec(),
+        _ => None,
+    }
+}
+
+/// Sign the provided buffer of data and return it in an SSH Certificiate
+/// signature formatted byte vector
+pub fn ssh_cert_signer(buf: &[u8]) -> Option<Vec<u8>> {
+    match sign_data(&buf, AlgorithmId::EccP256, SlotId::Retired(RetiredSlotId::R11)) {
+        Ok(signature) => {
+            let sig_type = "ecdsa-sha2-nistp256";
+            let mut encoded: Vec<u8> = (sig_type.len() as u32).to_be_bytes().to_vec();
+            encoded.extend_from_slice(sig_type.as_bytes());
+            let (r,s) = match rustica_sshkey::utils::asn_der_to_r_s(&signature) {
+                Some((r,s)) => (r, s),
+                None => return None,
+            };
+            let mut sig_encoding = vec![];
+            sig_encoding.extend_from_slice(&(r.len() as u32).to_be_bytes());
+            sig_encoding.extend_from_slice(r);
+            sig_encoding.extend_from_slice(&(s.len() as u32).to_be_bytes());
+            sig_encoding.extend_from_slice(s);
+
+            encoded.extend_from_slice(&(sig_encoding.len() as u32).to_be_bytes());
+            encoded.extend(sig_encoding);
+
+            Some(encoded)
+        },
+        Err(e) => {
+            println!("Error: {:?}", e);
+            None
+        },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,42 @@
 use std::collections::HashMap;
-use rustica_sshkey::{Certificate, PublicKey};
+use rustica_sshkey::{CertType, Certificate, PublicKey};
 
 mod hsm;
 
-use hsm::yubikey;
-
-fn test_pubkey() -> Option<Vec<u8>> {
-    let encoded_key = match yubikey::fetch_pubkey() {
-        //Ok(hsm::PublicKeyInfo::Rsa { pubkey, .. }) => pubkey,
-        Ok(hsm::PublicKeyInfo::EcP256(pubkey)) => {
-            let key_type = "ecdsa-sha2-nistp256";
-            let identifier = "nistp256";
-
-            let mut encoded: Vec<u8> = (key_type.len() as u32).to_be_bytes().to_vec();
-            encoded.extend_from_slice(key_type.as_bytes());
-
-            encoded.extend_from_slice(&(identifier.len() as u32).to_be_bytes());
-            encoded.extend_from_slice(identifier.as_bytes());
-
-            let pubkey = pubkey.as_bytes();
-            encoded.extend_from_slice(&(pubkey.len() as u32).to_be_bytes());
-            encoded.extend_from_slice(pubkey);
-
-            encoded
-        },
-        //Ok(hsm::PublicKeyInfo::EcP384(pubkey)) => pubkey.as_bytes().to_vec(),
-        _ =>  return None,
-    };
-
-    Some(encoded_key)
-}
-
-fn asn_der_to_r_s(buf: &[u8]) -> Option<(&[u8], &[u8])> {
-    if buf.len() < 4 ||  buf[0] != 0x30 {
-        println!("Bad formatting at the start");
-        return None;
-    }
-    let buf = &buf[3..];
-    let r_length = buf[0] as usize;
-    if buf.len() < r_length + 2 {
-        println!("Bad r len");
-        return None;
-    }
-    let buf = &buf[1..];
-    let r = &buf[..r_length];
-    let buf = &buf[r_length..];
-    if buf[0] != 0x2 {
-        println!("Bad Marker after r, found: {:x}", buf[0]);
-        return None
-    }
-    let s_length = buf[1] as usize;
-    let s = &buf[2..];
-
-    if s.len() != s_length {
-        println!("Bad s len");
-        return None
-    }
-
-    Some((r, s))
-}
-
-fn test_signer(buf: &[u8]) -> Option<Vec<u8>> {
-    match yubikey::sign_data(&buf) {
-        Ok(signature) => {
-            let sig_type = "ecdsa-sha2-nistp256";
-            let mut encoded: Vec<u8> = (sig_type.len() as u32).to_be_bytes().to_vec();
-            encoded.extend_from_slice(sig_type.as_bytes());
-            let (r,s) = match asn_der_to_r_s(&signature) {
-                Some((r,s)) => (r, s),
-                None => return None,
-            };
-            let mut sig_encoding = vec![];
-            sig_encoding.extend_from_slice(&(r.len() as u32).to_be_bytes());
-            sig_encoding.extend_from_slice(r);
-            sig_encoding.extend_from_slice(&(s.len() as u32).to_be_bytes());
-            sig_encoding.extend_from_slice(s);
-
-            encoded.extend_from_slice(&(sig_encoding.len() as u32).to_be_bytes());
-            encoded.extend(sig_encoding);
-
-            Some(encoded)
-        },
-        Err(e) => {
-            println!("Error: {:?}", e);
-            None
-        },
-    }
-}
+use hsm::yubikey::{ssh_cert_fetch_pubkey, ssh_cert_signer};
+use yubikey_piv::key::{RetiredSlotId, SlotId};
 
 fn main() {
-    let test_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLwHQFAZUIU+VjEsLqy04Vgbo1YUmRcZ6BybmeE3RMP6sl+ZAY4fsyGjEqIukcAFmXQ2LdGoooJsXm20KST7Vwc= obelisk@exclave.lan";
+    //let user_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLwHQFAZUIU+VjEsLqy04Vgbo1YUmRcZ6BybmeE3RMP6sl+ZAY4fsyGjEqIukcAFmXQ2LdGoooJsXm20KST7Vwc= obelisk@exclave.lan";
+    //let host_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBN8FRyW3AnkGZmm2BBlxYULPS6hpKNyrFfsghF09wyyKpJ0GvXfwYDyDaBR+SAIybXxO3m9+LKJqQh5D91pD/a8= root@ubuntu";
+    
+    let user_ca_cert = match ssh_cert_fetch_pubkey(SlotId::Retired(RetiredSlotId::R11)) {
+        Some(ca_cert) => ca_cert,
+        None => {
+            println!("Could not fetch public key from YubiKey. Is it configured?");
+            return;
+        },
+    };
 
-    let pkey = PublicKey::from_string(test_key).unwrap();
+    // Eventually this will be stored in another slot but for now, it's fine to keep
+    // the same for each
+    let host_ca_cert = match ssh_cert_fetch_pubkey(SlotId::Retired(RetiredSlotId::R11)) {
+        Some(ca_cert) => ca_cert,
+        None => {
+            println!("Could not fetch public key from YubiKey. Is it configured?");
+            return;
+        },
+    };
+
+    println!("User CA Pubkey: {}", user_ca_cert);
+    println!("User CA Fingerprint (SHA256): {}\n", user_ca_cert.fingerprint().hash);
+
+    println!("Host CA Pubkey: {}", host_ca_cert);
+    println!("Host CA Fingerprint (SHA256): {}\n", host_ca_cert.fingerprint().hash);
+
+    /*
+    // Generate User Cert
+    let pkey = PublicKey::from_string(user_key).unwrap();
     let mut extensions = HashMap::new();
     extensions.insert(String::from("permit-X11-forwarding"), String::from(""));
     extensions.insert(String::from("permit-agent-forwarding"), String::from(""));
@@ -99,21 +44,69 @@ fn main() {
     extensions.insert(String::from("permit-pty"), String::from(""));
     extensions.insert(String::from("permit-user-rc"), String::from(""));
 
-    let cert = Certificate::new(
+    let user_cert = Certificate::new(
         pkey,
+        CertType::User,
         0xFEFEFEFEFEFEFEFE,
         String::from("obelisk@exclave"),
-        vec![String::from("obelisk2")],
+        vec![String::from("obelisk")],
         0,
         0xFFFFFFFFFFFFFFFF,
         HashMap::new(),
         extensions,
-        test_pubkey,
-        test_signer,
+        user_ca_cert.clone(),
+        ssh_cert_signer,
     );
     
-    match cert {
-        Ok(cert) => println!("{}", cert),
+    match user_cert {
+        Ok(cert) => {
+            let serialized = format!("{}", cert);
+            if let Err(e) = Certificate::from_string(&serialized) {
+                println!("Couldn't deserialize certificate: {}", e);
+                return;
+            }
+            println!("Certificate Generated!");
+            println!("Info:");
+            println!("{:#}", cert);
+            println!("Export:");
+            println!("{}", cert);
+        },
         Err(e) => println!("Encountered an error while creating certificate: {}", e),
     }
+
+    println!("");
+
+    // Generate Host Cert
+    let pkey = PublicKey::from_string(host_key).unwrap();
+    
+    let host_cert = Certificate::new(
+        pkey,
+        CertType::Host,
+        0xFEFEFEFEFEFEFEFE,
+        String::from("atheris"),
+        vec![String::from("atheris")],
+        0,
+        0xFFFFFFFFFFFFFFFF,
+        HashMap::new(),
+        HashMap::new(),
+        host_ca_cert.clone(),
+        ssh_cert_signer,
+    );
+
+    match host_cert {
+        Ok(cert) => {
+            let serialized = format!("{}", cert);
+            if let Err(e) = Certificate::from_string(&serialized) {
+                println!("Couldn't deserialize certificate: {}", e);
+                return;
+            }
+            println!("Certificate Generated!");
+            println!("Info:");
+            println!("{:#}", cert);
+            println!("Export:");
+            println!("{}", cert);
+        },
+        Err(e) => println!("Encountered an error while creating certificate: {}", e),
+    }
+    */
 }
