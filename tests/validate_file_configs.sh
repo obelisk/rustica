1925f12d2b46273404560d45606ac8d4031501ef
Mitchell Grenier <mitchell@confurious.io>
2022-10-13 11:54:20 -0400

    Revert default features

--- tests/validate_file_configs.sh
+++ tests/validate_file_configs.sh
@@ -1,10 +1,10 @@
 # Exit when any command fails
 set -e
 
-cargo run --bin rustica -- -v --config examples/rustica_external.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file_alt.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file_multi.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file_with_influx.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file_with_splunk.toml
-cargo run --bin rustica -- -v --config examples/rustica_local_file_with_webhook.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_external.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_alt.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_multi.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_influx.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_splunk.toml
+cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_webhook.toml
