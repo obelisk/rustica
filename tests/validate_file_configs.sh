568cef6bee16aa9e88e649b0ffc7fd3d4519cf8b
Mitchell Grenier <mitchell@confurious.io>
2023-05-20 12:04:00 -0700

    Update configs

--- tests/validate_file_configs.sh
+++ tests/validate_file_configs.sh
@@ -2,9 +2,11 @@
 set -e
 
 cargo run --features=all --bin rustica -- -v --config examples/rustica_external.toml
+#cargo run --features=all --bin rustica -- -v --config examples/rustica_local_amazonkms.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_alt.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_multi.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_influx.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_splunk.toml
 cargo run --features=all --bin rustica -- -v --config examples/rustica_local_file_with_webhook.toml
+#cargo run --features=all --bin rustica -- -v --config examples/rustica_local_yubikey.toml
