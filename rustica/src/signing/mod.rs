328acfa9f616c3b33f22c4e665046f1f40be1250
Mitchell Grenier <mitchell@confurious.io>
2023-05-28 23:24:08 -0700

    Make x509 certificate optional as well

--- rustica/src/signing/mod.rs
+++ rustica/src/signing/mod.rs
@@ -64,7 +64,7 @@ pub trait Signer {
     /// Return the CA certificate used for signing X509 certificate requests.
     /// This function may hide away async code (as it does in the KMS signer)
     /// due to using the remote KeyPair trait imported from the rcgen crate
-    fn get_attested_x509_certificate_authority(&self) -> &rcgen::Certificate;
+    fn get_attested_x509_certificate_authority(&self) -> Option<&rcgen::Certificate>;
 
     /// Return the CA certificate used for signing X509 certificate requests.
     /// This function may hide away async code (as it does in the KMS signer)
@@ -171,6 +171,7 @@ impl std::fmt::Display for SigningMechanism {
                             .fingerprint()
                             .hash
                     ));
+
                     output.push_str(&format!(
                         "\tHost CA Fingerprint (SHA256): {}\n",
                         signer
@@ -179,10 +180,13 @@ impl std::fmt::Display for SigningMechanism {
                             .fingerprint()
                             .hash
                     ));
-                    output.push_str(&format!(
-                        "\tAttested X509 Certificate Authority:\n{}\n",
-                        signer.1.get_attested_x509_certificate_authority().serialize_pem().unwrap()
-                    ));
+
+                    if let Some(attested_x509_authority) = signer.1.get_attested_x509_certificate_authority() {
+                        output.push_str(&format!(
+                            "\tAttested X509 Certificate Authority:\n{}\n",
+                            attested_x509_authority.serialize_pem().unwrap()
+                        ));
+                    }
 
                     if let Some(client_certificate_authority) = signer.1.get_client_certificate_authority() {
                         output.push_str(&format!(
@@ -238,7 +242,7 @@ impl SigningMechanism {
     pub fn get_attested_x509_certificate_authority(
         &self,
         authority: &str,
-    ) -> Result<&rcgen::Certificate, SigningError> {
+    ) -> Result<Option<&rcgen::Certificate>, SigningError> {
         match &self.signing_system {
             SigningSystem::Internal(authorities) => {
                 if let Some(authority) = authorities.get(authority) {
