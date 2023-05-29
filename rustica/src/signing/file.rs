54eed74a10d8fa7b361c22a081a727a4bf039302
Mitchell Grenier <mitchell@confurious.io>
2023-05-29 01:38:33 -0700

    Bring full modularity to signing system

--- rustica/src/signing/file.rs
+++ rustica/src/signing/file.rs
@@ -13,11 +13,11 @@ use async_trait::async_trait;
 #[derive(Deserialize)]
 pub struct Config {
     /// The private key used to sign user certificates
-    #[serde(deserialize_with = "parse_private_key")]
-    user_key: PrivateKey,
+    #[serde(deserialize_with = "option_parse_private_key")]
+    user_key: Option<PrivateKey>,
     /// The private key used to sign host certificates
-    #[serde(deserialize_with = "parse_private_key")]
-    host_key: PrivateKey,
+    #[serde(deserialize_with = "option_parse_private_key")]
+    host_key: Option<PrivateKey>,
     /// X509 base64 encoded private key that will be used to issue certificates
     /// from the request_x509 API
     x509_private_key: Option<String>,
@@ -34,11 +34,16 @@ pub struct Config {
     client_certificate_authority_common_name: Option<String>,
 }
 
-pub struct FileSigner {
+pub struct SshKeys {
     /// The private key used to sign user certificates
-    user_key: PrivateKey,
+    user: PrivateKey,
     /// The private key used to sign host certificates
-    host_key: PrivateKey,
+    host: PrivateKey,
+}
+
+pub struct FileSigner {
+    /// The SSH keys used to sign SSH Certificate requests
+    ssh_keys: Option<SshKeys>,
     /// The public portion of the key that will be used to sign X509
     /// certificates
     x509_certificate: Option<X509Certificate>,
@@ -74,18 +79,21 @@ fn rcgen_certificate_from_private_key(
 #[async_trait]
 impl Signer for FileSigner {
     async fn sign(&self, cert: Certificate) -> Result<Certificate, SigningError> {
+        let keys = self.ssh_keys.as_ref().ok_or(SigningError::SignerDoesNotHaveSSHKeys)?;
         let final_cert = match cert.cert_type {
-            CertType::User => cert.sign(&self.user_key),
-            CertType::Host => cert.sign(&self.host_key),
+            CertType::User => cert.sign(&keys.user),
+            CertType::Host => cert.sign(&keys.host),
         };
 
         final_cert.map_err(|_| SigningError::SigningFailure)
     }
 
-    fn get_signer_public_key(&self, cert_type: CertType) -> PublicKey {
+    fn get_signer_public_key(&self, cert_type: CertType) -> Option<PublicKey> {
+        let keys = self.ssh_keys.as_ref()?;
+
         match cert_type {
-            CertType::User => self.user_key.pubkey.clone(),
-            CertType::Host => self.host_key.pubkey.clone(),
+            CertType::User => Some(keys.user.pubkey.clone()),
+            CertType::Host => Some(keys.host.pubkey.clone()),
         }
     }
 
@@ -121,22 +129,31 @@ impl SignerConfig for Config {
             _ => None,
         };
 
+        let ssh_keys = match (self.user_key, self.host_key) {
+            (Some(user), Some(host)) => Some(SshKeys{user, host}),
+            (None, None) => None,
+            _ => return Err(SigningError::SignerDoesNotAllRequiredSSHKeys),
+        };
+
         Ok(Box::new(FileSigner {
-            user_key: self.user_key,
-            host_key: self.host_key,
+            ssh_keys,
             x509_certificate,
             client_certificate_authority,
         }))
     }
 }
 
-fn parse_private_key<'de, D>(deserializer: D) -> Result<PrivateKey, D::Error>
+fn option_parse_private_key<'de, D>(deserializer: D) -> Result<Option<PrivateKey>, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
-    let key = String::deserialize(deserializer)?;
+    let key = match String::deserialize(deserializer) {
+        Ok(s) => s,
+        _ => return Ok(None),
+    };
+
     match PrivateKey::from_string(&key) {
-        Ok(key) => Ok(key),
+        Ok(key) => Ok(Some(key)),
         Err(e) => Err(serde::de::Error::custom(e.to_string())),
     }
 }
